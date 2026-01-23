98cf4b5a7eef5e8c90aaecb062e39d19912ad765
Vimal290704 <vimalyadavkr001@gmail.com>
2026-01-23 10:35:54 +0530

    added gitignore

--- services/identity-service/src/main/java/com/namora/identity/repository/AuthIdentityRepository.java
+++ services/identity-service/src/main/java/com/namora/identity/repository/AuthIdentityRepository.java
@@ -4,9 +4,10 @@ import com.namora.identity.entities.AuthIdentity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AuthIdentityRepository extends JpaRepository<AuthIdentity, UUID> {
-
+    Optional<AuthIdentity> findByEmail(String email);
 }
